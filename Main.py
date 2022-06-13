46539de7a331cdfcd9bb88d73666f626ecca8a33
zahil <zahilaty@gmail.com>
2022-06-13 22:48:18 +0300

    1) sync between the notebooks and my code 2) add meachanizim to AUC metric

--- Main.py
+++ Main.py
@@ -5,6 +5,12 @@ Created on Sun Apr 24 22:08:59 2022
 @author: zahil
 """
 
+#Only in the notebook
+# from google.colab import files
+# from google.colab import drive
+# drive.mount('/content/drive')
+# import os
+# print(os.getcwd())
 ### Imports ###
 import numpy as np
 import torch
@@ -13,10 +19,10 @@ from torch.utils.data import DataLoader
 from TrainAux import MyDataset,my_augmentations
 from helper_func import MyResNet18,DealWithOutputs,Net1D,Identity,GetResNet101
 import scipy.io as sio
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix,roc_auc_score
 
 ### HyperParams ###
-BATCH_SIZE = 512
+BATCH_SIZE = 1024
 EPOCHS = 0
 LEARNING_RATE = 0.0001
 mat_file = 'Data\DataV2_mul.mat' #TODO change data\ to os.join
@@ -49,7 +55,12 @@ if regression_or_classification == 'regression':
 if regression_or_classification == 'classification':
     #net  = MyResNet18(InputChannelNum=4,IsSqueezed=0,LastSeqParamList=[512,32,4],pretrained=True).cuda()
     net  = GetResNet101(InputChannelNum=4,LastSeqParamList=[2048,512,32,4],pretrained=True).cuda()
-    
+
+### A new try
+#net.fc[0][0].p = 0.9
+#net.fc[0][0].p = 0.75
+#net.fc[2][0].p = 0.5
+
 ### Creterion - I dont see any reason to use MSE and not MAE at this moment
 loss_fn = nn.L1Loss(reduction='none') 
 loss_fn_val = nn.L1Loss(reduction='none') 
@@ -62,6 +73,8 @@ optimizer = torch.optim.Adam(net.parameters(),lr=LEARNING_RATE)
 val_samples, val_labels ,val_weights = next(iter(val_dataloader)) #better practice
 targets_val = torch.reshape(val_labels,(-1,1)).float().cuda()
 val_weights = torch.reshape(val_weights,(-1,1)).float().cuda()
+y_true = targets_val.cpu().detach().numpy()
+y_true_bin = y_true>0.9
 
 ### The training loop
 Costs = np.array([])
@@ -70,7 +83,8 @@ Costs_val = np.array([])
 if LastCheckPoint is not None:
     net.load_state_dict(torch.load(LastCheckPoint)) 
 net.train() # In case we re-run this "cell"
-min_loss_val = 0.8
+min_loss_val = 0.55
+max_auc_val = 0.87
 #batch_i, [batch,labels,weights] = next(enumerate(train_dataloader)) #for debug
 for Epoch in range(EPOCHS):
     for batch_i, [batch,labels,weights] in enumerate(train_dataloader):
@@ -91,29 +105,39 @@ for Epoch in range(EPOCHS):
         net.eval()
         with torch.no_grad():
             outputs_val = net(val_samples)
+            
+            Probs = outputs_val/outputs_val.sum(axis=1,keepdims=True)
+            prob_preds = np.clip(torch.sum(Probs[:,1:],axis=1).cpu().numpy(),0.0,1.0)
+            AUC_val = roc_auc_score(y_true_bin, prob_preds)
+                          
             outputs_val = DealWithOutputs(regression_or_classification,outputs_val)
+            
             loss_val_weighted = torch.mean(loss_fn_val(outputs_val,targets_val)*val_weights) #should be AUC for bin case, and L1 (as now) for multy label
             loss_val = torch.mean(loss_fn_val(outputs_val,targets_val))
-            accuracy_bin = torch.sum((outputs_val>0.5) == torch.reshape(targets_val>0,(-1,1)).cuda())/len(val_set)
+            
+            
             Costs_val_weighted = np.append(Costs_val_weighted,loss_val_weighted.cpu().detach().numpy())
             Costs_val = np.append(Costs_val,loss_val.cpu().detach().numpy())
-            if loss_val.item() < min_loss_val:              
-                min_name = 'ResNet_' + str(round(Costs_val[-1],3))
+            
+            if (loss_val.item() < min_loss_val) or (AUC_val>max_auc_val):              
+                min_name = 'ResNet_' + str(round(Costs_val[-1],3) + '_' + str(AUC_val))
                 torch.save(net.state_dict(), min_name  + '.pth' )
                 min_loss_val -= 0.025
-                y_true = targets_val.cpu().detach().numpy()
+                max_auc_val += 0.1
                 predicted_method_1 = torch.round(outputs_val).reshape(-1,)
                 y_pred_1 = predicted_method_1.cpu().detach().numpy()
                 cf_matrix_1 = confusion_matrix(y_true,y_pred_1)
-                sio.savemat(min_name + '.mat', {"Costs": Costs, "Costs_val": Costs_val,'Costs_val_weighted': Costs_val_weighted,'cf_matrix_1':cf_matrix_1})
+                sio.savemat(min_name + '.mat', {"Costs": Costs, "Costs_val": Costs_val,'Costs_val_weighted': Costs_val_weighted,'cf_matrix_1':cf_matrix_1,'AUC':AUC_val})
         net.train()
-        print('[%d, %5d] loss: %.3f  Loss_val: %.3f Accuracy: %.1f' %(Epoch + 1, batch_i + 1, Costs[-1],Costs_val[-1],accuracy_bin*100))
+        print('[%d, %5d] loss: %.3f  Loss_val: %.3f AUC: %.1f' %(Epoch + 1, batch_i + 1, Costs[-1],Costs_val[-1],AUC_val))
 
 ### Save net weights
 print('Finished Training')
 #end_name =  'ResNet_' + str(round(Costs_val[-1],3)) + '.pth' #manualy write: 
 end_name = LastCheckPoint
 #torch.save(net.state_dict(),end_name)
+# myfile = min_name + '.pth' #only in the notebook
+# myfile2 = min_name + '.mat' #only in the notebook
 
 ### Classification Metric
 with torch.no_grad():
@@ -123,6 +147,11 @@ with torch.no_grad():
             #net.load_state_dict(torch.load(end_name)) #same as not doing anything
         net.eval()
         outputs_val = net(val_samples)
+        
+        Probs = outputs_val/outputs_val.sum(axis=1,keepdims=True)
+        prob_preds = np.clip(torch.sum(Probs[:,1:],axis=1).cpu().numpy(),0.0,1.0)
+        AUC_val = roc_auc_score(y_true_bin, prob_preds)
+            
         outputs_val = DealWithOutputs(regression_or_classification,outputs_val)
         predicted_method_1 = torch.round(outputs_val).reshape(-1,)
         
@@ -143,7 +172,6 @@ print(f'Accuracy of the network on the test images (estimator1): {100 * correct_
 print(f'Accuracy of the network on the test images (estimator2): {100 * correct_method_2 // len(val_set)} %')
 
 ### Confussion Matrix
-y_true = targets_val.cpu().detach().numpy()
 y_pred_1 = predicted_method_1.cpu().detach().numpy()
 y_pred_2 = predicted_method_2.cpu().detach().numpy()
 cf_matrix_1 = confusion_matrix(y_true,y_pred_1)
@@ -158,4 +186,11 @@ sio.savemat('LossVals.mat', {"Costs": Costs, "Costs_val": Costs_val,'Costs_val_w
                              'cf_matrix_1':cf_matrix_1,'cf_matrix_2':cf_matrix_2,
                              'outputs_val':outputs_val.cpu().detach().numpy(),
                              'Ensemble_out':Ensemble_out.cpu().detach().numpy(),
-                             'targets_val':targets_val.cpu().detach().numpy()})
+                             'targets_val':targets_val.cpu().detach().numpy(),
+                             'AUC':AUC_val})
+### Only in notebook
+# !cp $myfile /content/drive/MyDrive/ResNet101/
+# !cp $myfile2 /content/drive/MyDrive/ResNet101/
+# !cp $end_name /content/drive/MyDrive/ResNet101/
+# !cp 'LossVals.mat' /content/drive/MyDrive/ResNet101/
+# print(min_name)
